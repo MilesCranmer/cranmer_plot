1599a6d6fc600c8fad0853c01cad6b74b042fcd1
Miles Cranmer <miles.cranmer@gmail.com>
2019-02-15 17:50:41 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='cranmer_plot',
+    packages=[],
+    version='0.0.1',
+    description='Personal plotting scripts',
+    author='Miles Cranmer',
+    license='BSD',
+    author_email='miles.cranmer@gmail.com',
+    url='https://github.com/MilesCranmer/cranmer_plot',
+)
