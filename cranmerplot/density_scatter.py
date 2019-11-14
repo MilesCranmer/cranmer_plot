a956be992a35b5a0ce7a661ab141a508f179a799
Miles Cranmer <miles.cranmer@gmail.com>
2019-11-13 21:25:19 -0500

    Create density_scatter.py

---
+++ cranmerplot/density_scatter.py
@@ -0,0 +1,31 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
+plt.rc('font', family='serif')
+
+def density_scatter(x, y, xlabel='', ylabel='', clabel='Sample Density', log=False,
+    width_mult=1, bins=30, p_cut=0, update_rc=True):
+  if update_rc:
+    plt.rc('font', family='serif')
+    
+  fig, ax = plt.subplots(1, 1, figsize=(4*width_mult, 3))
+  xy = np.array([x, y]).T
+  px = xy[:, 0]
+  py = xy[:, 1]
+  p = p_cut
+  range_x = [np.percentile(xy[:, 0], i) for i in [p, 100-p]]
+  range_y = [np.percentile(xy[:, 1], i) for i in [p, 100-p]]
+  pxy = xy[(xy[:, 0] > range_x[0]) & (xy[:, 0] < range_x[1]) & (xy[:, 1] > range_y[0]) & (xy[:, 1] < range_y[1])]
+  px = pxy[:, 0]
+  py = pxy[:, 1]
+  norm = None
+  if log:
+    norm = LogNorm()
+
+  h, xedge, yedge, im = ax.hist2d(px, py, density=True, norm=norm, bins=[int(width_mult*bins), bins])
+  ax.set_ylabel(ylabel)
+  ax.set_xlabel(xlabel)
+  fig.colorbar(im).set_label(clabel)
+  fig.tight_layout()
+
+  return fig, ax
